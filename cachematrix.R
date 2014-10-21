f94fff54fdd06bd371fedc7e284f372721c8f767
abdulbaqi <sharaf.al.islam@gmail.com>
2014-10-21 13:13:57 +0300

    third with error

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,16 @@
 ## This function places the inverse of a matrix into cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse<-NULL
+  set <- function(y){
+    x<<- y
+    inverse<<- NULL
+  }
+  
+  get<- function() x
+  setinv <- function(inv) inverse <<- inv
+  getinv <- funciton() inverse
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 
 }
 
@@ -12,4 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <-x$getinv()
+  if(!is.null(inverse)){
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  data<-x$get()
+  inverse <-solve(data)
+  x$setinv(inverse)
+  inverse
 }
